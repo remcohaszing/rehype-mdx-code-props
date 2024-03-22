007ff4af2a2c11bfbdc7c3b592c247df35cbb076
Remco Haszing <remcohaszing@gmail.com>
2024-03-22 18:47:06 +0100

    Use hast-util-properties-to-mdx-jsx-attributes
    This replaces some reusable logic.

--- src/rehype-mdx-code-props.ts
+++ src/rehype-mdx-code-props.ts
@@ -1,13 +1,9 @@
-import { stringify as commas } from 'comma-separated-tokens'
-import { valueToEstree } from 'estree-util-value-to-estree'
 import { type Root } from 'hast'
+import { propertiesToMdxJsxAttributes } from 'hast-util-properties-to-mdx-jsx-attributes'
 import { fromMarkdown } from 'mdast-util-from-markdown'
 import { mdxFromMarkdown } from 'mdast-util-mdx'
 import { type MdxJsxFlowElementHast } from 'mdast-util-mdx-jsx'
 import { mdxjs } from 'micromark-extension-mdxjs'
-import { find, hastToReact, html } from 'property-information'
-import { stringify as spaces } from 'space-separated-tokens'
-import styleToObject from 'style-to-js'
 import { type Plugin } from 'unified'
 import { visitParents } from 'unist-util-visit-parents'
 
@@ -95,55 +91,9 @@ const rehypeMdxCodeProps: Plugin<[RehypeMdxCodePropsOptions?], Root> = ({
       replacement.children = child.children
       replacement.data = child.data
       replacement.position = child.position
-
-      // Processing attributes is strongly based on
-      // https://github.com/syntax-tree/hast-util-to-estree/blob/3.1.0/lib/handlers/element.js
-      for (let [name, value] of Object.entries(child.properties)) {
-        const info = find(html, name)
-
-        if (value == null || value === false || Number.isNaN(value) || (!value && info.boolean)) {
-          continue
-        }
-
-        name = (elementAttributeNameCase === 'react' && hastToReact[info.property]) || info.property
-
-        if (Array.isArray(value)) {
-          value = info.commaSeparated ? commas(value) : spaces(value)
-        } else if (typeof value === 'number') {
-          value = String(value)
-        }
-
-        if (value === true) {
-          replacement.attributes.unshift({
-            type: 'mdxJsxAttribute',
-            name,
-            value: null
-          })
-        } else if (name === 'style') {
-          replacement.attributes.unshift({
-            type: 'mdxJsxAttribute',
-            name,
-            value: {
-              type: 'mdxJsxAttributeValueExpression',
-              value,
-              data: {
-                estree: {
-                  type: 'Program',
-                  sourceType: 'module',
-                  body: [
-                    {
-                      type: 'ExpressionStatement',
-                      expression: valueToEstree(styleToObject.default(value))
-                    }
-                  ]
-                }
-              }
-            }
-          })
-        } else {
-          replacement.attributes.unshift({ type: 'mdxJsxAttribute', name, value })
-        }
-      }
+      replacement.attributes.unshift(
+        ...propertiesToMdxJsxAttributes(child.properties, { elementAttributeNameCase })
+      )
 
       parent.children[parent.children.indexOf(child)] = replacement
     })
